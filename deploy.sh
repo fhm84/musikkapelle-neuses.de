557709b1e8269b48d1d25b30176b7b2f51a0eddb
Fabian <fabianhalbmann@web.de>
2020-04-13 18:14:07 +0200

    env variables

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install sshpass rsync
 
-sshpass -p ${RSYNC_PASSWORD} ssh ${USERNAME}@web172.dogado.net
+sshpass -p $RSYNC_PASSWORD ssh $USERNAME@web172.dogado.net
 
-rsync -ruv --delete --rsh=ssh pulic ${USERNAME}@web172.dogado.net:/httpdocs
+rsync -ruv --delete --rsh=ssh $TRAVIS_BUILD_DIR/pulic $USERNAME@web172.dogado.net:/httpdocs
