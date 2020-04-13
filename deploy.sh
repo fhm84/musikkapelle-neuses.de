95c3ad1a86f15231096303fc0a73b36524a380ea
Fabian <fabianhalbmann@web.de>
2020-04-13 18:16:12 +0200

    install tools in travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-apt-get install sshpass rsync
-
 sshpass -p $RSYNC_PASSWORD ssh $USERNAME@web172.dogado.net
 
 rsync -ruv --delete --rsh=ssh $TRAVIS_BUILD_DIR/pulic $USERNAME@web172.dogado.net:/httpdocs
