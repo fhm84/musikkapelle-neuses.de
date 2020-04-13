e63100e5c9398e2c24a14684ecc80819b6ae7dfd
Fabian <fabianhalbmann@web.de>
2020-04-13 18:06:31 +0200

    use sshpass

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-apt-get install rsync
+apt-get install sshpass rsync
+
+sshpass -p ${RSYNC_PASSWORD} ssh ${USERNAME}@web172.dogado.net
 
 rsync -ruv --delete --rsh=ssh pulic ${USERNAME}@web172.dogado.net:/httpdocs
